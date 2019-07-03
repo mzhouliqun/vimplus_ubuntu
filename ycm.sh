2a3f98de3e473ce150ff06b4b68976c93f8b6049
Zhou Liqun <mzhouliqun@gmail.com>
2019-07-04 01:05:05 +0800

    Fixed logic errors in the YCM installation script

--- ycm.sh
+++ ycm.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 #apt purge --auto-remove vim
 apt-get update
-apt-get install gcc g++ makev -y
+apt-get install gcc g++ make -y
 apt-get install vim vim-gtk3 vim-tiny neovim vim-athena vim-gtk vim-nox -y
 apt-get install python-pip curl git libncurses5-dev python-dev python3-dev build-essential cmake clang libclang-dev -y
 # 安装vim-plug插件管理器
+
+FILENAME=/root/.vimrc
+STRING='\" YouCompleteMe'
+turnon() {
+	sed -i '/\" YouCompleteMe/,$s/^\"//' $1
+}
+
+turnoff() {
+	sed -i '/\" YouCompleteMe/,$s/^.*/\"&/' $1
+}
+
+str=$(cat $FILENAME | grep plug\#end | awk '{print substr($1,1,1)}')
+if [ "$str" != "\"" ]
+then
+	turnoff $FILENAME
+fi
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sed -i '/\" YouCompleteMe/,$s/^\"//' /root/.vimrc
+turnon $FILENAME
 echo '\033[32mFrom the vim command line, run the "PlugInstall" command to install YCM.\033[0m'
 echo '\033[32mIf installation fails, execute "/root/.vim/plugged/YouCompleteMe/install.py to reinstall the YCM plug-in"\033[0m'
